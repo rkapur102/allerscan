a0cd7c59b083fe1415740a42cc3d2bb296ea3ca4
eschiller04 <60821362+eschiller04@users.noreply.github.com>
2021-02-06 13:52:04 -0500

    Update ContentView.swift

--- Allergy/ContentView.swift
+++ Allergy/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Commit test :P")
-            .padding()
-    }
-}
+    @State var allergies: String = ""
+    @State private var selection = 1
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        NavigationView {
+            ScrollView {
+            VStack() {
+                Text("Welcome! Please choose your language and enter your allergies. ")
+                Picker(selection: $selection, label: Text("Languages:")) {
+                                Text("English").tag(1)
+                                Text("Spanish").tag(2)
+                                Text("French").tag(3)
+                            }
+                TextField("Enter allergies here (i.e. Eggs, Fish, Milk...)", text: $allergies)
+                let array = allergies.components(separatedBy: ", ")
+                Text("Your allergies: ")
+                List(array, id: \.self) { string in
+                    Text(string)
+                }
+            }
+            .padding()
+        }
+        }
+     /*   NavigationLink(*destination: CameraView()) {
+            Text("Take Ingredient List Photo!")
+            .padding()
+        }*/
     }
+
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
 }
