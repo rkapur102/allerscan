14e61d3b64ec6916282e86561b44b5edca6a14b4
Rhea Kapur <rhea.kapur1@gmail.com>
2021-02-06 11:07:11 -0500

    Initial Commit

---
+++ Allergy/AllergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllergyApp.swift
+//  Allergy
+//
+//  Created by Rhea Macbook on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct AllergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Allergy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Allergy
+//
+//  Created by Rhea Macbook on 2/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
